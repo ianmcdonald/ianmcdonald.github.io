37204129af6da3a5003c4a0aa6e95a906fbf37b5
Ian Mcdonald <iangregoryjames@gmail.com>
2016-09-06 22:29:38 -0400

    fix master

--- src/links.js
+++
@@ -1,7 +0,0 @@
-const Urls = [
-  {id: 1, url: '//github.com/ianmcdonald', text: 'GitHub'},
-  {id: 2, url: '//twitter.com/ianmcdonald_', text: 'Twitter'},
-  {id: 3, url: 'mailto:iangregoryjames@gmail.com', text: 'Email'}
-];
-
-export default Urls;
