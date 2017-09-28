2baf9085b9e3d53de971c7866765e1cb456b75d3
Ian McDonald <iangregoryjames@gmail.com>
2017-09-28 02:29:35 -0400

    day/night

---
+++ js/index.js
@@ -0,0 +1,27 @@
+(function() {
+
+  var inverter = document.querySelector('.inverter')
+  var magicks = document.querySelectorAll('.magick svg')
+  var hours = new Date().getHours()
+
+  if (hours && (hours < 7 || hours > 20))
+    document.body.className = 'invert';
+  
+  function magick(x) {
+    for (var i = 0; i < magicks.length; i++) {
+      magicks[i].id = ''
+    }
+    magicks[x].id = 'opaque';
+    x < magicks.length - 1 ? x += 1 : x = 0;
+    return setTimeout(function() { magick(x) }, 1200)
+  }
+  
+  inverter.addEventListener('click', function() {
+    document.body.className !== 'invert' ?
+      document.body.className = 'invert' :
+      document.body.className = ''
+  });
+  
+  magick(0)
+  
+})();
